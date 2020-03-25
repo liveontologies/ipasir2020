543a3f4afc20d329ecd29bd6481737f58a5af4d1
Yevgeny Kazakov <yevgeny.kazakov@uni-ulm.de>
2020-03-26 00:17:59 +0100

    Add cryptominisat v 5.6.8

---
+++ sat/cryptominisat5/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -x
+
+rm -rf bin/crypto*
+rm -rf bin/cusp*
+
+rm -rf build
+mkdir build
+
+(
+cd build || exit
+cmake -DENABLE_PYTHON_INTERFACE=OFF -DNOVALGRIND=ON -DNOZLIB=ON -DONLY_SIMPLE=ON -DSTATICCOMPILE=ON -DIPASIR=ON -DCMAKE_BUILD_TYPE=Release ..
+make -j4 VERBOSE=1
+)
